ab04cb80340f0bcba8d95caa6f935a060eeb3270
RoccoZero <RoccoZero@outlook.com>
2026-04-17 15:06:14 +0300

    added target netstandard2.0

---
+++ src/System.Net.Http.Handlers/ArgumentNullException.cs
@@ -0,0 +1,20 @@
+﻿#if !NET6_0_OR_GREATER
+namespace System;
+
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.CompilerServices;
+
+internal static class ArgumentNullExceptionExtensions
+{
+    extension(ArgumentNullException)
+    {
+        public static void ThrowIfNull([NotNull] object? argument, [CallerArgumentExpression("argument")] string? paramName = null)
+        {
+            if (argument is null)
+            {
+                throw new ArgumentNullException(paramName);
+            }
+        }
+    }
+}
+#endif
